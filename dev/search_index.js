3fd7d626f2d73ffe95ac7abca9b462543e08ae36
Documenter.jl <documenter@juliadocs.github.io>
2024-01-24 18:17:36 +0000

    build based on d1d735e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RBPmotifsCore","category":"page"},{"location":"#RBPmotifsCore","page":"Home","title":"RBPmotifsCore","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RBPmotifsCore.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RBPmotifsCore]","category":"page"}]
+}
